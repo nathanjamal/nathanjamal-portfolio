719c1a88e1d4f59e008d731cf0d99e8d1b9b15a5
nathan <nathanjamal@ymail.com>
2014-02-24 15:50:42 +0000

    added css min to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,16 +2,42 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
+    
     pkg: grunt.file.readJSON('package.json'),
 
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> - Burgerverse Industries */\n'
       },
       build: {
         src: ['assets/js/plugins.js','assets/js/base.js'],
         dest: 'assets/js/base.min.js'
       }
+    },
+
+    cssmin: {
+
+      minify: {
+        expand: true,
+        cwd: 'assets/stylesheets/',
+        src: ['*.css', '!*.min.css'],
+        dest: 'assets/stylesheets/',
+        ext: '.min.css'
+      },
+
+      // combine: {
+      //   files: {
+      //     'assets/stylesheets/combined.css': ['assets/stylesheets/screen.min.css']
+      //   }
+      // },
+
+      add_banner: {
+        options: {
+          banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> - Burgerverse Industries */\n'
+        },
+        files: 'assets/stylesheets/combined.css'
+      }
+
     }
 
   });
@@ -19,7 +45,10 @@ module.exports = function(grunt) {
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['uglify', 'cssmin']);
+
 
 };
